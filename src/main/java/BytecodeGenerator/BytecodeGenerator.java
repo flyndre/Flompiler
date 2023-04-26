9de6e19085f4786b5652e55de311b208257955ae
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2023-04-26 11:12:33 +0200

    Updated AbstrakteSyntax

--- src/main/java/BytecodeGenerator/BytecodeGenerator.java
+++ src/main/java/BytecodeGenerator/BytecodeGenerator.java
@@ -1,7 +1,18 @@
 package BytecodeGenerator;
 
-public class BytecodeGenerator {
-    public void generateByteCode(){
+import ScannerParserLexer.Syntaxtree.Class;
+import ScannerParserLexer.Syntaxtree.Program;
+import jdk.internal.org.objectweb.asm.ClassWriter;
 
+import java.util.ArrayList;
+
+public class BytecodeGenerator {
+    public void generateByteCode(Program program){
+        ArrayList<Class> classes = (ArrayList<Class>) program.classes;
+
+        for(int i=0; i< classes.size();i++){
+            ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
+            
+        }
     }
 }
