dc61727a32f299edcefe04ff102a5bdca84c91dc
David Maier <i21018@hb.dhbw-stuttgart.de>
2023-05-03 11:01:09 +0200

    test: add tests for ScannerParserLexer/EmptyClass

--- src/test/java/de/flyndre/flompiler/FlompilerTest.java
+++ src/test/java/de/flyndre/flompiler/FlompilerTest.java
@@ -1,46 +1,5 @@
 package de.flyndre.flompiler;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+public class FlompilerTest {
 
-/**
- * Unit test for simple App.
- */
-public class FlompilerTest extends TestCase {
-    /**
-     * Create the test case
-     * @param testName name of the test case
-     */
-    public FlompilerTest(String testName) {
-        super(testName);
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(FlompilerTest.class);
-    }
-
-    /**
-     * Rigorous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
-    }
-
-    /**
-     * Parse empty class.
-     */
-    public void testEmptyClass() {
-        final var path = TestConstants.RESOURCES_ROOT + "/basic/EmptyClass.java";
-    }
-
-    /**
-     * Try to parse defective empty class and expect exception.
-     */
-    public void testDefectiveEmptyClass() {
-        final var path = TestConstants.RESOURCES_ROOT + "/basic/defective/DefectiveEmptyClass.java";
-    }
 }
