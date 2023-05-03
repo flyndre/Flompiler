bff8e406853b97b2eed99e9b8290119336cb2da1
Ruben Kraft <i21015@hb.dhbw-stuttgart.de>
2023-05-03 09:54:29 +0200

    feat: add functionality in main

--- src/main/java/Flompiler.java
+++ src/main/java/Flompiler.java
@@ -1,4 +1,7 @@
-import java.io.File;
+import de.flyndre.flompiler.scannerparserlexer.ScannerParserLexer;
+import de.flyndre.flompiler.scannerparserlexer.syntaxtree.Program;
+
+import java.io.*;
 import java.net.URI;
 
 /**
@@ -11,7 +14,7 @@ public class Flompiler {
      * Compiles a .java file to a .class file.
      * @param args The arguments of the compiler, the first of which must be the path to the input file.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length < 1) {
             throw new RuntimeException("Please specify an input file as first argument.");
         }
@@ -26,6 +29,16 @@ public class Flompiler {
         );
         System.out.println(outputFile.getAbsolutePath());
 
+        BufferedReader objReader = new BufferedReader(new FileReader(inputFile));
+
+        String strCurrentLine;
+        String input = "";
+
+        while ((strCurrentLine = objReader.readLine()) != null) {
+            input += strCurrentLine;
+
+        }
+        Program program = ScannerParserLexer.compile(input);
         // TODO: ScannerParserLexer
 
         // TODO: Syntax
