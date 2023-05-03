d40e2cc19241d2fb4fc20286492839efc438e665
Paul Lehmann <i21017@hb.dhbw-stuttgart.de>
2023-05-03 10:46:08 +0200

    added code for generating constructors

--- src/main/java/de/flyndre/flompiler/bytecodegenerator/BytecodeGenerator.java
+++ src/main/java/de/flyndre/flompiler/bytecodegenerator/BytecodeGenerator.java
@@ -1,9 +1,7 @@
 package de.flyndre.flompiler.bytecodegenerator;
 
+import de.flyndre.flompiler.scannerparserlexer.syntaxtree.*;
 import de.flyndre.flompiler.scannerparserlexer.syntaxtree.Class;
-import de.flyndre.flompiler.scannerparserlexer.syntaxtree.Field;
-import de.flyndre.flompiler.scannerparserlexer.syntaxtree.Method;
-import de.flyndre.flompiler.scannerparserlexer.syntaxtree.Program;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
@@ -85,14 +83,26 @@ public class BytecodeGenerator {
         return cw;
     }
 
+    public ClassWriter generateByteCodeForMethods(ClassWriter cw, List<Method> methods){
+        //get all methods without constructors
+        List<Method> methodsWithoutConstructors = new ArrayList<>();
+        for(int i=0;i<methods.size();i++){
+            if(!methods.get(i).name.equals("<init>")){
+                methodsWithoutConstructors.add(methods.get(i));
+            }
+        }
+
+        return cw;
+    }
+
     public ClassWriter generateByteCodeForConstructors(ClassWriter cw, List<Method> methods, List<Field> fields){
         //get all fields with default values
-        List<Field> defaultFields = new ArrayList<>();
+        /*List<Field> defaultFields = new ArrayList<>();
         for(int i=0;i<fields.size();i++){
             if(!fields.get(i).standardValue.equals(null)){
                 defaultFields.add(fields.get(i));
             }
-        }
+        }*/
 
         //get all constructors out of the methods
         List<Method> constructors = new ArrayList<>();
@@ -103,9 +113,69 @@ public class BytecodeGenerator {
         }
 
         //generate the bytecode for the constructors
-        /*for(int i=0;i<constructors.size();i++){
-            MethodVisitor
-        }*/
+        for(int i=0;i<constructors.size();i++){
+            Method thisConstructor = constructors.get(i);
+
+            //generate constructor method descriptor
+            ////generate the parameters
+            List<Parameter> params = thisConstructor.parameter;
+            String parametersDescriptor = "";//if list is empty, empty method descriptor
+            for(int a=0; a<params.size();a++){
+                switch(params.get(a).type){
+                    case "int":
+                        parametersDescriptor = parametersDescriptor + "I";
+                        break;
+                    case "boolean":
+                        parametersDescriptor = parametersDescriptor + "Z";
+                        break;
+                    case "char":
+                        parametersDescriptor = parametersDescriptor + "C";
+                        break;
+                    default:
+                        parametersDescriptor = parametersDescriptor + "L" + params.get(a).type + ";";
+                        break;
+                }
+            }
+
+            ////generate the return value descriptor
+            String returnDescriptor = "";
+            switch(thisConstructor.type){
+                case "int":
+                    returnDescriptor = "I";
+                    break;
+                case "boolean":
+                    returnDescriptor = "Z";
+                    break;
+                case "char":
+                    returnDescriptor = "C";
+                    break;
+                case "void":
+                    returnDescriptor = "V";
+                    break;
+                default:
+                    returnDescriptor = "L" + thisConstructor.type + ";";
+                    break;
+            }
+            ////generate the actual descriptor
+            String descriptor = "(" + parametersDescriptor + ")" + returnDescriptor;
+            MethodVisitor consMeth = cw.visitMethod(Opcodes.ACC_PUBLIC, "<init>", descriptor, null, null);
+            consMeth.visitCode();
+            //hier kommt der code für initialisierung im Kontruktor
+            consMeth.visitVarInsn(Opcodes.ALOAD, 0);
+            consMeth.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+
+            //Aufruf des normalen generateByteCodeForStatements
+            consMeth = generateByteCodeForStatements(consMeth, thisConstructor.statement);
+
+            consMeth.visitInsn(Opcodes.RETURN);
+            consMeth.visitMaxs(0,0);
+            consMeth.visitEnd();
+        }
         return cw;
+    }
+
+    public MethodVisitor generateByteCodeForStatements(MethodVisitor mv, Statement statements){
+
+        return mv;
     }
 }
