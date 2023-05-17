6688cca40d715efae2f9ed7648851b4f44011cb8
Lukas Burkhardt <i21005@hb.dhbw-stuttgart.de>
2023-05-17 11:12:16 +0200

    update type check methods

--- src/main/java/de/flyndre/flompiler/scannerparserlexer/syntaxtree/Parameter.java
+++ src/main/java/de/flyndre/flompiler/scannerparserlexer/syntaxtree/Parameter.java
@@ -1,5 +1,7 @@
 package de.flyndre.flompiler.scannerparserlexer.syntaxtree;
 
+import java.util.List;
+
 /**
  * 
  */
@@ -24,5 +26,8 @@ public class Parameter {
     public Parameter(String name, String type){
         this.type = type;
         this.name = name;
+    }
+    public String typeCheck(List<Field> fields, List<Parameter> parameters) throws Exception {
+        return type;
     }
 }
