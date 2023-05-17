a17ee6bdbd17f38dfae47dce0fb7d5563696c2b0
David Maier <i21018@hb.dhbw-stuttgart.de>
2023-05-17 15:02:41 +0200

    tests: added manual tests for unused method parameters

--- manual-test/Test.java
+++ manual-test/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String args[]) {
-        var x = new StaticIfElse().method();
+        var x = new BooleanMethod().method(true);
         System.out.println(x);
     }
 }
