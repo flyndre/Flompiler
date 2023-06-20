8f2c9e151c6b0555a037865c201dac31704ea9a0
David Maier <i21018@hb.dhbw-stuttgart.de>
2023-06-20 15:35:36 +0200

    tests: implement assertion methods

--- src/test/java/de/flyndre/flompiler/testing/Flassertions.java
+++ src/test/java/de/flyndre/flompiler/testing/Flassertions.java
@@ -1,11 +1,11 @@
 package de.flyndre.flompiler.testing;
 
 import com.google.gson.Gson;
-import de.flyndre.flompiler.scannerparserlexer.syntaxtree.Method;
 import de.flyndre.flompiler.scannerparserlexer.syntaxtree.Program;
 import org.junit.jupiter.api.Assertions;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * Assertions used to test the Flompiler.
@@ -34,18 +34,22 @@ public class Flassertions {
     }
 
     public static void assertClassDeclaresField(Object instance, Field field) {
-        assertClassDeclaresFields(instance, new Field[]{ field });
+
     }
 
     public static void assertClassDeclaresFields(Object instance, Field[] fields) {
-        // TODO: loop fields to check whether non-null fields on it match with any fields of instance
+        for (Field f : fields) {
+            assertClassDeclaresField(instance, f);
+        }
     }
 
     public static void assertClassDeclaresMethod(Object instance, Method method) {
-        assertClassDeclaresMethods(instance, new Method[]{ method });
+
     }
 
     public static void assertClassDeclaresMethods(Object instance, Method[] methods) {
-        // TODO: loop methods to check whether non-null fields on it match with any methods of instance
+        for (Method m : methods) {
+            assertClassDeclaresMethod(instance, m);
+        }
     }
 }
