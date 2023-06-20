ea1967376cc14203eaff76dc53db75dfd93e64e3
David Maier <i21018@hb.dhbw-stuttgart.de>
2023-06-20 11:43:05 +0200

    tests: create bytecodegenerator test

--- src/test/java/de/flyndre/flompiler/bytecodegenerator/BytecodeGeneratorTest.java
+++ src/test/java/de/flyndre/flompiler/bytecodegenerator/BytecodeGeneratorTest.java
@@ -4,4 +4,44 @@ import junit.framework.TestCase;
 
 public class BytecodeGeneratorTest extends TestCase {
 
+    /**
+     * Prepares the compilation of the given file.
+     * @param inputAst the input typed AST
+     * @param outputFilePath where the resulting `.class` file should be placed
+     * @return an executable which attempts to compile the file
+     */
+    private Executable createBytecodeGeneratorExecutable(Program inputAst, String outputFilePath) {
+        var outputFile = new File(TestConstants.RESOURCES_ROOT + outputFilePath);
+        return () -> BytecodeGenerator.generateByteCode(inputAst, outputFile);
+    }
+
+    /**
+     * Attempts to transform the given AST into a java program.
+     * If successful, the compilation produces a `.class` file next to the given `.java` file.
+     * @param inputAst the input typed AST
+     * @param outputFilePath where the resulting `.class` file should be placed
+     */
+    private void testParsingSuccess(Program inputAst, String outputFilePath) {
+        Assertions.assertDoesNotThrow(createBytecodeGeneratorExecutable(inputAst, outputFilePath));
+    }
+
+    /**
+     * Tries to transform the given AST and expects the compilation to fail.
+     * @param inputAst the input typed AST
+     * @param outputFilePath where the resulting `.class` file should be placed
+     */
+    private void testParsingFailure(Program inputAst, String outputFilePath) {
+        Assertions.assertThrows(Exception.class, createBytecodeGeneratorExecutable(inputAst, outputFilePath));
+    }
+
+
+
+    // EMPTY CLASS
+
+    @Test
+    @DisplayName("TypeChecker: Empty Class")
+    public void testEmptyClass() {
+        testSuccess(EmptyClassResults.TYPED_AST, "/basic/EmptyClass.class");
+    }
+
 }
